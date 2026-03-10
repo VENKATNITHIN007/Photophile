267727d26f36107462f3f21c88a9937d813653dc
Venkatnithin007 <Venkatnithin36@gmail.com>
2026-03-10 09:52:03 +0530

    feat(auth): integrate navbar into layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
 import { Toaster } from "@/components/ui/sonner";
+import { Navbar } from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
+          <Navbar />
           <Toaster position="top-right" richColors />
           {children}
         </AuthProvider>
